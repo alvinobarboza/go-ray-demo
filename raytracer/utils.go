4ec48596a8d8a1de28e3074dbeef3afd4120ccaf
Alvino Barboza <alvinobarboza@gmail.com>
2025-07-24 13:47:31 -0300

    fix - typo

--- raytracer/utils.go
+++ raytracer/utils.go
@@ -30,9 +30,9 @@ func VecNormal(v rl.Vector3) rl.Vector3 {
 
 func VecAdd(v1, v2 rl.Vector3) rl.Vector3 {
 	return rl.Vector3{
-		X: v1.X + v1.X,
-		Y: v1.Y + v1.Y,
-		Z: v1.Z + v1.Z,
+		X: v1.X + v2.X,
+		Y: v1.Y + v2.Y,
+		Z: v1.Z + v2.Z,
 	}
 }
 
